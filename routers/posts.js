770dd8cdca4ea2445f53f4afa1d514a55a82d316
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-03-27 14:19:04 +0100

    feat: bonus 2 completed

--- routers/posts.js
+++ routers/posts.js
@@ -8,36 +8,43 @@ const posts = require('../data/posts.js');
 const router = express.Router();
 
 //index
-router.get('/', (req, res) =>{
+router.get('/', (req, res) => {
     //la risposta sarà la lista dei post in json 
     res.json(posts);
 })
 
 //show
-router.get('/:slug', (req, res) =>{
+router.get('/:slug', (req, res) => {
     let element = req.params.slug;
-    res.send(`Dettagli del post: ${element}.`);
+    //non essendo presente un id faccio riferimento allo slug presente
+    let post = posts.find(post => {
+        if (element === post.slug) {
+            return post;
+        }
+    })
+    //la risposta sarà il singolo oggetto (il post singolo che verrà visualizzato) convertito in json
+    res.json(post);
 })
 
 //store
-router.post('/', (req, res) =>{
+router.post('/', (req, res) => {
     res.send('Creazione di un nuovo post');
 })
 
 //update
-router.put('/:slug', (req, res) =>{
+router.put('/:slug', (req, res) => {
     let element = req.params.slug;
     res.send(`Modifica integrale del post: ${element}.`);
 })
 
 //modify
-router.patch('/:slug', (req, res) =>{
+router.patch('/:slug', (req, res) => {
     let element = req.params.slug;
     res.send(`Modifica parziale del post: ${element}.`);
 })
 
 //destroy
-router.delete('/:slug', (req, res) =>{
+router.delete('/:slug', (req, res) => {
     let element = req.params.slug;
     res.send(`Eliminazione del post: ${element}.`);
 })
