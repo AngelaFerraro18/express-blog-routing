cc53af42d7f84a756a982cf47a460cf02e553fcc
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-03-27 13:27:45 +0100

    feat: import posts.js from data and router

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+//importo express
 const express = require('express');
+
+//importo router
+const postsRouter = require('./routers/posts.js')
+
+//importo l'array con i post
+const posts = require('./data/posts.js');
 const app = express();
 const port = 3000;
 
+//home del blog
 app.get('/', (req, res) =>{
-    res.send('Server del mio blog');
+    res.send('Server del mio blog con vari post');
 })
+
+//bacheca
+app.use("/posts", postsRouter);
 
 app.listen(port, () =>{
     console.log(`Server in ascolto alla porta: ${port}`);
